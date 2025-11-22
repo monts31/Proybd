22d457ba2801f993a90f0edc65109304173134be
callmefrancia <s23120184@alumnos.itsur.edu.mx>
2025-11-21 19:50:44 -0600

    Menu inic

---
+++ Proybd/Frontend/Menu.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Proybd.Frontend
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            label1 = new Label();
+            lblFecha = new Label();
+            lblEmpleado = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            textBox1 = new TextBox();
+            pic1 = new PictureBox();
+            pic2 = new PictureBox();
+            pic4 = new PictureBox();
+            pic3 = new PictureBox();
+            pic8 = new PictureBox();
+            pic7 = new PictureBox();
+            pic6 = new PictureBox();
+            pic5 = new PictureBox();
+            dataGridView1 = new DataGridView();
+            btnRechazar = new Button();
+            btnConfirmar = new Button();
+            btnIZQ = new Button();
+            btnDer = new Button();
+            ((System.ComponentModel.ISupportInitialize)pic1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // lblFecha
+            // 
+            lblFecha.AutoSize = true;
+            lblFecha.BackColor = Color.Transparent;
+            lblFecha.Font = new Font("Palatino Linotype", 27.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblFecha.Location = new Point(203, 29);
+            lblFecha.Name = "lblFecha";
+            lblFecha.Size = new Size(108, 51);
+            lblFecha.TabIndex = 1;
+            lblFecha.Text = "Fecha";
+            // 
+            // lblEmpleado
+            // 
+            lblEmpleado.AutoSize = true;
+            lblEmpleado.BackColor = Color.Transparent;
+            lblEmpleado.Font = new Font("Palatino Linotype", 27.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblEmpleado.Location = new Point(607, 29);
+            lblEmpleado.Name = "lblEmpleado";
+            lblEmpleado.Size = new Size(169, 51);
+            lblEmpleado.TabIndex = 2;
+            lblEmpleado.Text = "Empleado";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(335, 46);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(224, 23);
+            dateTimePicker1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(802, 46);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(179, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // pic1
+            // 
+            pic1.Location = new Point(78, 106);
+            pic1.Name = "pic1";
+            pic1.Size = new Size(223, 173);
+            pic1.TabIndex = 5;
+            pic1.TabStop = false;
+            // 
+            // pic2
+            // 
+            pic2.Location = new Point(352, 106);
+            pic2.Name = "pic2";
+            pic2.Size = new Size(223, 173);
+            pic2.TabIndex = 6;
+            pic2.TabStop = false;
+            // 
+            // pic4
+            // 
+            pic4.Location = new Point(893, 106);
+            pic4.Name = "pic4";
+            pic4.Size = new Size(223, 173);
+            pic4.TabIndex = 8;
+            pic4.TabStop = false;
+            // 
+            // pic3
+            // 
+            pic3.Location = new Point(621, 106);
+            pic3.Name = "pic3";
+            pic3.Size = new Size(223, 173);
+            pic3.TabIndex = 7;
+            pic3.TabStop = false;
+            pic3.Click += pictureBox4_Click;
+            // 
+            // pic8
+            // 
+            pic8.Location = new Point(893, 308);
+            pic8.Name = "pic8";
+            pic8.Size = new Size(223, 173);
+            pic8.TabIndex = 12;
+            pic8.TabStop = false;
+            // 
+            // pic7
+            // 
+            pic7.Location = new Point(621, 308);
+            pic7.Name = "pic7";
+            pic7.Size = new Size(223, 173);
+            pic7.TabIndex = 11;
+            pic7.TabStop = false;
+            // 
+            // pic6
+            // 
+            pic6.Location = new Point(352, 308);
+            pic6.Name = "pic6";
+            pic6.Size = new Size(223, 173);
+            pic6.TabIndex = 10;
+            pic6.TabStop = false;
+            // 
+            // pic5
+            // 
+            pic5.Location = new Point(78, 308);
+            pic5.Name = "pic5";
+            pic5.Size = new Size(223, 173);
+            pic5.TabIndex = 9;
+            pic5.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(49, 507);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(1086, 151);
+            dataGridView1.TabIndex = 13;
+            // 
+            // btnRechazar
+            // 
+            btnRechazar.Location = new Point(378, 664);
+            btnRechazar.Name = "btnRechazar";
+            btnRechazar.Size = new Size(132, 32);
+            btnRechazar.TabIndex = 14;
+            btnRechazar.Text = "button1";
+            btnRechazar.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirmar
+            // 
+            btnConfirmar.Location = new Point(670, 664);
+            btnConfirmar.Name = "btnConfirmar";
+            btnConfirmar.Size = new Size(132, 32);
+            btnConfirmar.TabIndex = 15;
+            btnConfirmar.Text = "button2";
+            btnConfirmar.UseVisualStyleBackColor = true;
+            // 
+            // btnIZQ
+            // 
+            btnIZQ.Location = new Point(12, 259);
+            btnIZQ.Name = "btnIZQ";
+            btnIZQ.Size = new Size(60, 60);
+            btnIZQ.TabIndex = 16;
+            btnIZQ.Text = "button3";
+            btnIZQ.UseVisualStyleBackColor = true;
+            // 
+            // btnDer
+            // 
+            btnDer.Location = new Point(1123, 259);
+            btnDer.Name = "btnDer";
+            btnDer.Size = new Size(60, 60);
+            btnDer.TabIndex = 17;
+            btnDer.Text = "button4";
+            btnDer.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1195, 732);
+            Controls.Add(btnDer);
+            Controls.Add(btnIZQ);
+            Controls.Add(btnConfirmar);
+            Controls.Add(btnRechazar);
+            Controls.Add(dataGridView1);
+            Controls.Add(pic8);
+            Controls.Add(pic7);
+            Controls.Add(pic6);
+            Controls.Add(pic5);
+            Controls.Add(pic4);
+            Controls.Add(pic3);
+            Controls.Add(pic2);
+            Controls.Add(pic1);
+            Controls.Add(textBox1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(lblEmpleado);
+            Controls.Add(lblFecha);
+            Controls.Add(label1);
+            Name = "Menu";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)pic1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lblFecha;
+        private Label lblEmpleado;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox1;
+        private PictureBox pic1;
+        private PictureBox pic2;
+        private PictureBox pic4;
+        private PictureBox pic3;
+        private PictureBox pic8;
+        private PictureBox pic7;
+        private PictureBox pic6;
+        private PictureBox pic5;
+        private DataGridView dataGridView1;
+        private Button btnRechazar;
+        private Button btnConfirmar;
+        private Button btnIZQ;
+        private Button btnDer;
+    }
+}
